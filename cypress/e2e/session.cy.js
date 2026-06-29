6d115af0b6c01551c3329604cbc7302bd0e1ac46
Boualem nouari <Boualem.nouari92@gmail.com>
2026-06-29 22:53:14 +0200

    check les champs vide

--- cypress/e2e/session.cy.js
+++ cypress/e2e/session.cy.js
@@ -1,7 +1,8 @@
 describe("clique “Se connecter” sans rien saisir", () => {
   it("ne passe pas", () => {
     cy.visit("/login");
-    cy.url().should("include", "/login");
+    cy.get('[data-testid="signin-username"]').should("have.value", "");
+    cy.get('[data-testid="signin-password"]').should("have.value", "");
     cy.get('[data-testid="signin-submit"]').click();
     cy.url().should("include", "/login");
   });
