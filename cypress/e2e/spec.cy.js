40b3fe066ef9d47353d1fabfebd388336827a810
Boualem nouari <Boualem.nouari92@gmail.com>
2026-06-29 21:06:13 +0200

    eviter les doublon dans le code

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
 describe("template spec", () => {
-  it("passes", () => {
+  it.skip("passes", () => {
     cy.visit("/login");
     cy.get('[data-testid="signin-username"]').type("johndoe");
     cy.get('[data-testid="signin-password"]').type("s3cret");
@@ -9,11 +9,32 @@ describe("template spec", () => {
     cy.url().should("include", "/dashboard");
   });
 });
-it("ne passes pas ", () => {
+it.only("ne passes pas ", () => {
   cy.visit("/login");
   cy.get('[data-testid="signin-username"]').type("johndoe");
   cy.get('[data-testid="signin-password"]').type("wrongpassword123");
   cy.get('[data-testid="signin-submit"]').click();
   //cy.get('[data-testid="signin-password"]').type("s3cret{enter}");une autre methode de mettre un MP et valider
   cy.url().should("include", "/login");
+});
+//éviter de répéter le code
+describe("Login", () => {
+  beforeEach(() => {
+    cy.visit("/login");
+    cy.get('[data-testid="signin-username"]').type("johndoe");
+  });
+
+  it("Connexion réussie", () => {
+    cy.get('[data-testid="signin-password"]').type("s3cret");
+    cy.get('[data-testid="signin-submit"]').click();
+
+    cy.url().should("include", "/dashboard");
+  });
+
+  it.only("Connexion refusée", () => {
+    cy.get('[data-testid="signin-password"]').type("wrongpassword123");
+    cy.get('[data-testid="signin-submit"]').click();
+
+    cy.url().should("include", "/login");
+  });
 });
