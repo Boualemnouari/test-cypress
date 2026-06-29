f769642850bcf337cfe988ebb426546a6806e7e6
Boualem nouari <Boualem.nouari92@gmail.com>
2026-06-29 15:33:39 +0200

    connexion

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,7 +2,7 @@ describe("template spec", () => {
   it("passes", () => {
     cy.visit("/login");
     cy.get('[data-testid="signin-username"]').type("johndoe");
-    cy.get('[data-testid="signin-password"]').type("password123");
+    cy.get('[data-testid="signin-password"]').type("s3cret");
     cy.get('[data-testid="signin-button"]').click();
   });
 });
