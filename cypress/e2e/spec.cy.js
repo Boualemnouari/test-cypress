ea702b5064800f85f4c3fa377996e26ae889fe25
Boualem nouari <Boualem.nouari92@gmail.com>
2026-06-29 20:29:07 +0200

    correction

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,6 +6,6 @@ describe("template spec", () => {
     cy.get('[data-testid="signin-submit"]').click();
     //cy.get('[data-testid="signin-password"]').type("s3cret{enter}");une autre methode de mettre un MP et valider
     cy.url().should("not.include", "/login");
-    cy.url().should("include", "/dashbord");
+    cy.url().should("include", "/dashboard");
   });
 });
