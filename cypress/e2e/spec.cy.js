98749c094818af27ebf055f6a351f3e86f376772
Boualem nouari <Boualem.nouari92@gmail.com>
2026-06-29 20:20:02 +0200

    verifier la passage sur une autre page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,6 +4,8 @@ describe("template spec", () => {
     cy.get('[data-testid="signin-username"]').type("johndoe");
     cy.get('[data-testid="signin-password"]').type("s3cret");
     cy.get('[data-testid="signin-submit"]').click();
-    //cy.get('[data-testid="signin-password"]').type("s3cret{enter}");
+    //cy.get('[data-testid="signin-password"]').type("s3cret{enter}");une autre methode de mettre un MP et valider
+    cy.url().should("not.include", "/login");
+    cy.url().should("include", "/dashbord");
   });
 });
